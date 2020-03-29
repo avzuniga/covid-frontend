fa84b5dd179fd81bf14e2bd607788d606bbe78bd
Mauricio Maldonado Iturriago <mauriciomaldo@hotmail.es>
2020-03-29 17:55:19 -0500

    configurarición del store

--- src/router/index.js
+++ src/router/index.js
@@ -18,4 +18,17 @@ const router = new VueRouter({
     routes
 })
 
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (store.getters.isLoggedIn) {
+            next()
+            return
+        }
+        next('/Home')
+    } else {
+        next()
+    }
+})
+
+
 export default router
