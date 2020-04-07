28549a2cc932425d69bc6d8672199a25b2caf8d8
lkbarreto <lkbarreto@uninorte.edu.co>
2020-04-07 15:23:18 -0500

    conflicts fixed

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+    <v-navigation-drawer v-model="drawer" app temporary color="grey lighten-4">
+      <v-list dense class="grey lighten-4">
+        <!-- <CardAyuda/> --> <!-- Comentado para probar la card de las solicitudes -->
+        <CardSolicitud/>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <v-container fluid></v-container>
+      <v-row class="nav mx-auto">
+        <v-app-bar-nav-icon app @click="drawer = !drawer" />
+        <v-spacer />
+        <v-btn class="navButton">Iniciar Sesion</v-btn>
+        <v-btn class="white--text navButton" color="navbar">Registrar</v-btn>
+      </v-row>
+      
+    </v-content>
+  </div>
+</template>
+
+<script>
+import CardAyuda from '../components/Card_Ayuda'
+import CardSolicitud from '../components/Card_Solicitud'
+export default {
+  name: "Home",
+  data: () => ({
+    drawer: null
+  }),
+  components: {
+    CardAyuda,
+    CardSolicitud,
+  }
+};
+</script>
+<style>
+#keep .v-navigation-drawer__border {
+  display: none;
+}
+.navButton {
+  margin-left: 5px;
+  margin-right: 5px;
+}
+.nav {
+  background-color: transparent;
+}
+
+</style>
